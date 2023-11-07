424c8bd5370f6aac2a871427432aaf4bbd46ae60
TheAlem <josealem03@gmail.com>
2023-11-06 23:01:43 -0400

    probando

--- lib/Home/pages/posts_list.dart
+++ lib/Home/pages/posts_list.dart
@@ -1,11 +1,10 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:flutter_app_cats/Home/pages/cat_detail_screen.dart';
 
 class PostsList extends StatelessWidget {
   const PostsList({super.key});
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -14,44 +13,64 @@ class PostsList extends StatelessWidget {
         if (!snapshot.hasData) {
           return const CircularProgressIndicator();
         }
-        
+
         var posts = snapshot.data?.docs;
 
         return ListView.builder(
-          itemCount: posts?.length??0,
+          itemCount: posts?.length ?? 0,
           itemBuilder: (context, index) {
             var post = posts![index].data() as Map<String, dynamic>;
 
-            return Card(
-              margin: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ListTile(
-                    title: Text('Dueño: ${post['nombre_dueño']}'),
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CatDetailScreen(catData: post),
                   ),
-                  if (post['image_url'] != null) 
-                    Image.file(
-                      File(post['image_url']),
-                      width: double.infinity,
-                      height: 200.0,
-                      fit: BoxFit.cover,
+                );
+              },
+              child: Card(
+                margin: const EdgeInsets.all(16.0),
+                shape: RoundedRectangleBorder(
+                  // Bordes redondeados
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                elevation: 10, // Sombra en la parte inferior del Card
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      title: Text('Dueño: ${post['nombre_dueño']}'),
                     ),
-                
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Nombre del Gato: ${post['nombre']}'),
-                        Text('Descripción: ${post['descripcion']}'),
-                        Text('Sexo: ${post['sexo']}'),
-                        Text('Edad: ${post['edad']}'),
-                        Text('Teléfono del Dueño: ${post['telefono']}'),
-                      ],
+                    if (post['image_url'] != null &&
+                        post['image_url'].isNotEmpty)
+                      ClipRRect(
+                        // Clip the image with rounded corners
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(10.0)),
+                        child: Image.network(
+                          post['image_url'],
+                          width: double.infinity,
+                          height: 200.0,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Nombre del Gato: ${post['nombre']}'),
+                          Text('Descripción: ${post['descripcion']}'),
+                          Text('Sexo: ${post['sexo']}'),
+                          Text('Edad: ${post['edad']}'),
+                          Text('Teléfono del Dueño: ${post['telefono']}'),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             );
           },
