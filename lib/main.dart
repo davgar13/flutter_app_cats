993c92292b126f67bec45a93fd74242aa5d3697d
“rodripaz45” <rodripaz45@gmail.com>
2023-11-09 14:54:21 -0400

    Patron Bloc Rodri Paz

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_app_cats/Home/AppBar.dart';
+import 'package:flutter_app_cats/bloc/form_bloc.dart';
 import 'package:flutter_app_cats/firebase_options.dart';
 import 'package:flutter_app_cats/login_pages/login_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() async{
-  
+void main() async {
   runApp(const MyApp());
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -16,13 +20,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Login and Register',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginScreen(),
-    );
+    return BlocProvider<FormBloc>(
+        create: (context) => FormBloc(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Login and Register',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: LoginScreen(),
+        ));
   }
 }
